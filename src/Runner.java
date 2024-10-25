ef4d8ed28d90743a61613cb64471ee4ffa83f755
BT_1N3_30 <sample@bths.edu>
2024-10-25 10:04:15 -0400

    initial commit added some logic added some print statements

--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,8 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("What's up");
+        System.out.println("Hello");
+        System.out.println("Good bye");
+
     }
 }
